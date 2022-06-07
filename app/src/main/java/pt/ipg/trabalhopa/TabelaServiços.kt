28b606dd2e38746bb3171e6be9b65f7bad4b2c62
Francisco Loureiro <loureiro.francisco60@gmail.com>
2022-06-07 11:11:55 +0100

    criação da tabela serviços e inserção dos campos na mesma tabela

---
+++ app/src/main/java/pt/ipg/trabalhopa/TabelaServiços.kt
@@ -0,0 +1,22 @@
+package pt.ipg.trabalhopa
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaServiços(db: SQLiteDatabase):TabelaBD(db, NOME_TABELA) {
+    override fun cria() {
+        db.execSQL(
+            "CREATE TABLE $NOME_TABELA(${BaseColumns._ID}INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "$CAMPO_NOME TEXT NOT NULL," +
+                    "$CAMPO_DATA TEXT NOT NULL,"+
+                    "FOREIGN KEY ($CAMPO_CARRO_ID) REFERENCES ${TabelaCarros.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)"
+        )
+    }
+
+    companion object {
+        const val NOME_TABELA = "serviços"
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_DATA = "data"
+        const val CAMPO_CARRO_ID = "carroID"
+    }
+}
