72deabff19b39e0476c598a465ac179002d17de6
Francisco Loureiro <loureiro.francisco60@gmail.com>
2022-06-16 22:59:12 +0100

    alteração do nome dos fragmentos para nomes mais intuitivos

--- app/src/main/java/pt/ipg/trabalhopa/FragmentService.kt
+++ app/src/main/java/pt/ipg/trabalhopa/FragmentService.kt
@@ -6,12 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import pt.ipg.trabalhopa.databinding.FragmentServicosBinding
 
-import pt.ipg.trabalhopa.databinding.FragmentThirdBinding
+class FragmentService: Fragment() {
 
-class ThirdFragment: Fragment() {
-
-    private var _binding: FragmentThirdBinding? = null
+    private var _binding: FragmentServicosBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,7 +21,7 @@ class ThirdFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        _binding = FragmentServicosBinding.inflate(inflater, container, false)
         return binding.root
 
     }
